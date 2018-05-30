bcaa2486d2b281ea944384d27d05a302dcdc3a03
mwhitcom <mwhitcom@gmail.com>
2018-05-29 20:21:32 -0700

    updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,5 @@
 import React from 'react';
 
-const Home = () => (
-  <div>
-    <img
-      src="https://qy7s3400-a.akamaihd.net/wp-content/uploads/2016/02/IMG_4714-650x488.jpg"
-      alt="Goobi"
-    />
-    <h1>My naem is Goobi and I write gud. hai</h1>
-  </div>
-);
+const Home = () => <div>Hello</div>;
 
 export default Home;
